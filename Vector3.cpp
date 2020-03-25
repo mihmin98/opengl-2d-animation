13f9a6f94802ab06e12b970edadaa419cd737315
mihmin98 <mihmin98@yahoo.com>
2020-03-25 12:12:05 +0200

    Created Vector3 class

---
+++ Vector3.cpp
@@ -0,0 +1,63 @@
+#include "Vector3.hpp"
+
+Vector3::Vector3(float x = 0, float y = 0, float z = 0)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+std::string Vector3::ToString()
+{
+    std::string s = "(" + std::to_string(this->x) + ", " +
+                    std::to_string(this->y) + ", " +
+                    std::to_string(this->z) + ")";
+    return s;
+}
+
+float Vector3::DotProduct(const Vector3 &a, const Vector3 &b)
+{
+    return a.x * b.x + a.y * b.y + a.z + b.z;
+}
+
+Vector3 Vector3::CrossProduct(const Vector3 &a, const Vector3 &b)
+{
+    Vector3 result = Vector3();
+    result.x = a.y * b.z - a.z * b.y;
+    result.y = a.z * b.x - a.x * b.z;
+    result.z = a.x * b.y - a.x * b.x;
+    return result;
+}
+
+Vector3 &Vector3::operator+=(const Vector3 &rhs)
+{
+    this->x += rhs.x;
+    this->y += rhs.y;
+    this->z += rhs.z;
+    return *this;
+}
+
+Vector3 &Vector3::operator-=(const Vector3 &rhs)
+{
+    this->x -= rhs.x;
+    this->y -= rhs.y;
+    this->z -= rhs.z;
+    return *this;
+}
+
+bool operator==(const Vector3 &a, const Vector3 &b)
+{
+    return a.x == b.x && a.y == b.y && a.z == b.z;
+}
+
+Vector3 operator+(Vector3 lhs, const Vector3 &rhs)
+{
+    lhs += rhs;
+    return lhs;
+}
+
+Vector3 operator-(Vector3 lhs, const Vector3 &rhs)
+{
+    lhs -= rhs;
+    return lhs;
+}
---
+++ Vector3.hpp
@@ -0,0 +1,22 @@
+#include <string>
+
+class Vector3
+{
+public:
+    float x;
+    float y;
+    float z;
+
+    Vector3(float x, float y, float z);
+    std::string ToString();
+
+    static float DotProduct(const Vector3 &a, const Vector3 &b);
+    static Vector3 CrossProduct(const Vector3 &a, const Vector3 &b);
+
+    Vector3 &operator+=(const Vector3 &rhs);
+    Vector3 &operator-=(const Vector3 &rhs);
+
+    friend bool operator==(const Vector3 &a, const Vector3 &b);
+    friend Vector3 operator+(Vector3 lhs, const Vector3 &rhs);
+    friend Vector3 operator-(Vector3 lhs, const Vector3 &rhs);
+};
